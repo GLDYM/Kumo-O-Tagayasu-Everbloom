563758a337dafa1c3d7d708be02d249e2378a2c5
Polaris_Light <995905922@qq.com>
2026-06-13 23:09:03 +0800

    bump version
    Signed-off-by: Polaris_Light <995905922@qq.com>

--- .pakku/overrides/kubejs/server_scripts/kubejs_tags.js
+++ .pakku/overrides/kubejs/server_scripts/kubejs_tags.js
@@ -206,7 +206,7 @@ ServerEvents.tags('item', event => {
 // 巧克力
 ServerEvents.tags('item', event => {
   event.add('c:bars/chocolate', ['#c:chocolate'])
-  event.add('c:chocolate', ['#c:bars/chocolate'])
+  // event.add('c:chocolate', ['#c:bars/chocolate'])
 })
 
 // 切片
@@ -308,11 +308,11 @@ ServerEvents.tags('item', event => {
   event.add('c:raw_fish', ['#c:raw_fishes'])
   event.add('c:cooked_fish', ['#c:cooked_fishes'])
   event.add('c:fermentation', ['#c:crops/grape'])
-  event.add('c:crops/cantaloupe', ['#c:crops/hamimelon'])
+  // event.add('c:crops/cantaloupe', ['#c:crops/hamimelon'])
   event.add('c:crops/hamimelon', ['#c:crops/cantaloupe'])
-  event.add('c:fruits/cantaloupe', ['#c:fruits/hamimelon'])
+  // event.add('c:fruits/cantaloupe', ['#c:fruits/hamimelon'])
   event.add('c:fruits/hamimelon', ['#c:fruits/cantaloupe'])
-  event.add('c:wheat', ['#c:crops/wheat', '#c:grain/wheat'])
+  // event.add('c:wheat', ['#c:crops/wheat', '#c:grain/wheat'])
   event.add('c:crops/wheat', ['#c:wheat'])
   event.add('c:grain/wheat', ['#c:wheat'])
   event.add('c:food/raw_pasta', ['farmersdelight:raw_pasta', 'vintagedelight:cucumber_noodles'])
